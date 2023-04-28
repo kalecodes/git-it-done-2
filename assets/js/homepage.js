1ff475910bf8dcc4b0b80fec69c00c5982820d91
kalen510 <kalen.wiley@gmail.com>
2023-04-27 23:05:51 -0400

    display user repos and issues

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,49 @@
+// variables to capture user form input
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+// form inputs to display results
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
 
+var displayRepos = function(repos, searchTerm) {
+    // clear old content
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+
+    // loop over repos data
+    for (var i = 0; i < repos.length; i++) {
+        // format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        // create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.className = "list-item flex-row justify-space-between  align-center";
+
+        // create span elelment to hold repo name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        // append to container
+        repoEl.appendChild(titleEl);
+
+        // create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        // check if current repo has issues or not
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML = "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+
+        // append to container
+        repoEl.appendChild(statusEl);
+
+        // append container to DOM
+        repoContainerEl.appendChild(repoEl);
+    }
+};
 
 var getUserRepos = function(user) {
     // format the github api url
@@ -7,11 +52,23 @@ var getUserRepos = function(user) {
     // make a request to the url
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
-            console.log(data)
+            displayRepos(data, user);
         });
     });
 };
 
+var fromSubmitHandler = function(event) {
+    event.preventDefault();
+    
+    // get value from input element
+    var username = nameInputEl.value.trim();
 
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+}
 
-getUserRepos("microsoft");
+userFormEl.addEventListener("submit", fromSubmitHandler);
